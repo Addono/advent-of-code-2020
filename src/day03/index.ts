23c6297ce46783ac930f613bcef242cc7c884033
Adriaan Knapen <hi@aknapen.nl>
2020-12-03 09:20:21 +0100

    feat: adds solution for day 3

---
+++ src/day03/index.ts
@@ -0,0 +1,30 @@
+import { exit } from "process"
+import { sum, loadDataAsStringArray, multiply } from '../util';
+
+const data: string[] = loadDataAsStringArray('data/day03.txt')
+
+const anglePart1: [number, number] = [3, 1]
+
+const amountOfHits = (forrest: String[], [xAngle, yAngle]: [number, number]): number => {
+  return forrest
+    .filter((_, index) => (index % yAngle) === 0) // Remove all rows we jumped over
+    .map((value, index) => value[(xAngle * index) % value.length]) // Get the character for each row
+    .map(char => char === "#" ? 1 : 0) // See if the character is a tree
+    .reduce(sum, 0) // Count the amount of trees
+}
+
+console.log("Day 3", "Part 1", amountOfHits(data, anglePart1))
+
+const anglesPart2: [number, number][] = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+]
+
+const part2: number = anglesPart2
+  .map(angle => amountOfHits(data, angle))
+  .reduce(multiply, 1)
+
+console.log("Day 3", "Part 2", part2)
